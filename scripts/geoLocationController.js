730700e1b51987814d392edd4556e82a56078c8a
Ali Rokni <alirokni@gmail.com>
2015-09-12 16:55:36 -0700

    Updated ajax call and other minor changes
    updated to use loop for ajax call

--- scripts/geoLocationController.js
+++ scripts/geoLocationController.js
@@ -4,7 +4,7 @@ myGeoLocationApp.initialCityWeatherReport();
 // on dom ready main functions
 $(function () {
   $('#js-geo-btn').on('click', myGeoLocationApp.findMyLocationWeather);
-  $('#js-geo-search').on('click', myGeoLocationApp.searchLocationEnter);
+  $('#js-geo-search, #js-location-search').on('click', myGeoLocationApp.searchLocationEnter);
   var setMessageTimerMain = new myGeoLocationApp.SetMessageTimer('.information .js-geolocation-note', 'bg-warning', 3000);
   setMessageTimerMain.showMessage();
 });
